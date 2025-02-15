8235845d2afc75ca4d76ff12e667ca5200c71c1c
Bhanu <bhanupallapothula55@gmail.com>
2025-02-15 15:29:04 +0530

    feat : Added a program which prints odd numbers

---
+++ P5.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+class P5 {
+    public static void main(String[] args){
+        int i;
+        System.out.println("the odd numbers are :");
+        for(i=0;i<=100;i++){
+        if(i%2 != 0){
+            System.out.println(i+ "");
+        }
+       } 
+    }
+}
